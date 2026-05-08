4ec300236a4c4c948eb256307d4c001fef6838ab
umerAB-111 <umerabdenn@gmail.com>
2026-05-08 17:41:54 +0200

    Clean up - original layout restored

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Navbar from '../navigation/Navbar'
 import ThreeEnvironment from '../three/ThreeEnvironment'
-import ErrorBoundary from '../ui/ErrorBoundary'
 
 function Layout() {
   const { theme } = useSelector((state) => state.ui)
@@ -13,14 +12,8 @@ function Layout() {
   }, [theme])
 
   return (
-    <div className="min-h-screen bg-bg-primary relative">
-      <div className="fixed inset-0 overflow-hidden pointer-events-none">
-        <div className="absolute -top-40 -right-40 w-80 h-80 bg-accent-cyan/10 rounded-full blur-3xl" />
-        <div className="absolute -bottom-40 -left-40 w-96 h-96 bg-accent-purple/10 rounded-full blur-3xl" />
-      </div>
-      <ErrorBoundary>
-        <ThreeEnvironment />
-      </ErrorBoundary>
+    <div className="min-h-screen bg-bg-primary relative overflow-hidden">
+      <ThreeEnvironment />
       <Navbar />
       <main>
         <Outlet />
